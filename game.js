aeaa58ae9b4d4b9f88632e95e3205ad7c24c4bf2
MarcusFongKJ <65365866+MarcusFongKJ@users.noreply.github.com>
2024-04-23 12:03:28 +0800

    updated js to have user button interactions to include sounds and animation

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const buttonColours = ['red', 'blue', 'green', 'yellow'];
-const gamePattern = [];
+let gamePattern = [];
+const allBtns = $('.btn');
+let userClickedPattern = [];
+
 
 // Generate a number between 0 and 4
 function newSequence() {
@@ -11,3 +14,25 @@ function newSequence() {
 let randomChosenColour = buttonColours[newSequence()];
 gamePattern.push(randomChosenColour);
 
+// Flash the color
+let display = $('#' + randomChosenColour);
+display.fadeOut(100).fadeIn(100);
+
+// Display color sound
+playSound(randomChosenColour);
+
+
+// Event listener when user clicks on a button
+for (let i = 0; i < allBtns.length; i++) {
+    allBtns[i].addEventListener('click', function() {
+        let userChosenColour = allBtns[i].id;
+        userClickedPattern.push(userChosenColour);
+        playSound(userChosenColour);
+        console.log(userClickedPattern);    //to remove
+    });
+}
+
+function playSound(name) {
+    let sound = new Audio('./sounds/' + name + '.mp3');
+    sound.play();
+}
